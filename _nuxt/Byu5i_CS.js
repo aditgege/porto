999337d7e06a269be89e39aa800c7a0d96a3dfe6
aditgege <32119685+aditgege@users.noreply.github.com>
2025-10-27 11:21:36 +0000

    Deploying to gh-pages from @ aditgege/porto@fae58013be0f3d6ac009419e8a494969a4a96e55 🚀

---
+++ _nuxt/Byu5i_CS.js
@@ -0,0 +1,1 @@
+import{r as t}from"./TlOWdyGJ.js";import e from"./7_kDgLOK.js";import{y as n,E as s,h as a,w as i,I as p}from"./DU2muJjp.js";import"./iik6CYzq.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
