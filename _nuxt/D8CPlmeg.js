c3c33c0189a2aab3c2f5effd734579ec2266f76b
aditgege <32119685+aditgege@users.noreply.github.com>
2025-10-29 13:02:18 +0000

    Deploying to gh-pages from @ aditgege/porto@004732b142342e26cc7b373105a8a57f1a1ed0aa 🚀

--- _nuxt/D8CPlmeg.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./TlOWdyGJ.js";import t from"./7_kDgLOK.js";import{y as e,E as a,h as p,w as c,I as n}from"./DU2muJjp.js";import"./iik6CYzq.js";const d={__name:"ProseTip",setup(i){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
