a41751276d6472af59e9e4a937dca6006ef00371
adit <aditgege0@gmail.com>
2025-10-29 21:08:12 +0700

    feat: new menu

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,17 +1,17 @@
 export default defineAppConfig({
   global: {
     picture: {
-      dark: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      dark: '/me/gelap.jpg',
+      light: '/me/gelap.jpg',
       alt: 'My profile picture'
     },
-    meetingLink: 'https://cal.com/',
-    email: 'ui-pro@nuxt.com',
+    meetingLink: 'emailto:aditgege@gmail.com',
+    email: 'aditgege@gmail.com',
     available: true
   },
   ui: {
     colors: {
-      primary: 'blue',
+      primary: 'lime',
       neutral: 'neutral'
     },
     pageHero: {
@@ -25,21 +25,19 @@ export default defineAppConfig({
   footer: {
     credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
     colorMode: false,
-    links: [{
-      'icon': 'i-simple-icons-discord',
-      'to': 'https://go.nuxt.com/discord',
-      'target': '_blank',
-      'aria-label': 'Nuxt on Discord'
-    }, {
-      'icon': 'i-simple-icons-x',
-      'to': 'https://go.nuxt.com/x',
-      'target': '_blank',
-      'aria-label': 'Nuxt on X'
-    }, {
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt/ui',
-      'target': '_blank',
-      'aria-label': 'Nuxt UI on GitHub'
-    }]
+    links: [
+      {
+        'icon': 'i-simple-icons-instagram',
+        'to': 'https://instagram.com/aditgege',
+        'target': '_blank',
+        'aria-label': 'Instagram Profile'
+      },
+      {
+        'icon': 'i-simple-icons-github',
+        'to': 'https://github.com/aditgege',
+        'target': '_blank',
+        'aria-label': 'GitHub Profile'
+      }
+    ]
   }
 })
